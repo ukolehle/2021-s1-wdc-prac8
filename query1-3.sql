1512d8be0599d6786f83188da84b48583e5f5fa5
ukolehle <kolehd@gmail.com>
2021-05-29 08:27:45 +0000

    selects longest loan by order by and limit

--- query1-3.sql
+++ query1-3.sql
@@ -1,4 +1,8 @@
 SELECT
-    CONCAT(last_name, ' ', first_name)
-FROM actor
+    CONCAT(customer.first_name, ' ', customer.last_name) AS cust_name,
+    rental.rental_date
+FROM rental
+    JOIN customer ON rental.customer_id=customer.customer_id
+ORDER BY (rental.return_date)
+LIMIT 1;
 ;
